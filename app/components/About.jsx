bcd1f87d0b4779a7f0556d5132283b76b72f2322
Matthew Chick <chikmatthew@gmail.com>
2017-08-30 13:31:19 +1000

    Style about page & link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,13 +1,38 @@
 var React = require('react');
 
-/*
+
+
+/* method 2
 React 0.14 introduced functional, stateless components
 as a way to define React components as a function, rather than
 as an ES2015 class or via React.createClass.
 */
 var About = (props) => {
   return (
-    <h3>About component</h3>
+    <div>
+      <h1 className="text-center">About</h1>
+      <p>
+        This is a weather application build on React under Node.js. I have built this
+        for The Complete React Web App Developer Course.
+      </p>
+      <p>
+        Here are some of the tools I used:
+      </p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a>
+          - This was JavaScript framework used.
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a>
+          - Use Open Weather Map API to search for weather data by City name.
+        </li>
+        <li>
+          <a href="https://github.com/mochajs/mocha">Mocha</a>
+          - Use Mocha with Expect and Promise for testing.
+        </li>
+      </ul>
+     </div>
   )
 }
 /* method 1
